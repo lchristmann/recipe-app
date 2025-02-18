27572db9729b40d6f283bf4cb7666c28b31d7916
Leander Christmann <leander.christmann@proton.me>
2025-02-18 11:28:31 +0100

    remove global component footer since only one usage

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Banner from '@/components/Banner.vue';
 import Navbar from '@/components/Navbar.vue';
+import Footer from '@/components/Footer.vue';
 </script>
 
 <template>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from '@/App.vue'
 import router from '@/router'
-import Footer from '@/components/Footer.vue'
 
 createApp(App)
     .use(router)    // Install Vue Router
-    .component('Footer', Footer) // Register global component
     .mount('#app')      // Mount the app
